dca836907d54e3aff65568f13c1e21e71592e81c
DeFreitas-Samuel <52897285+DeFreitas-Samuel@users.noreply.github.com>
2020-11-09 14:21:54 -0400

    distribuccion de grupos y creacion de archivo con los grupos anadido

--- Program.cs
+++ Program.cs
@@ -25,19 +25,83 @@ namespace Organizador_de_equipos
                 Console.WriteLine("El programa se va a cerrar");
                 Environment.Exit(0);
             }
-            
+
+            Console.WriteLine("Introduzca la cantidad de equipos que desea: ");
+            int nroDeEquipos = Convert.ToInt32(Console.ReadLine());
+
+            int miembroPorEquipoSinSobrante = (estudiantes.Count / nroDeEquipos);
+            int temasPorEquipoSinSobrante = (temas.Count / nroDeEquipos);
+            int miembroSobrantes = (estudiantes.Count % nroDeEquipos);
+            int temaSobrantes = (temas.Count % nroDeEquipos);
+
+            Console.WriteLine($"Este es el numero de integrantes que deberia tener cada equipo: {miembroPorEquipoSinSobrante}");
+            Console.WriteLine($"Este es el numero de temas que deberia tener cada equipo: {temasPorEquipoSinSobrante}");
+            Console.WriteLine($"Este es el numero de personas que sobran: {miembroSobrantes}");
+            Console.WriteLine($"Este es el numero de temas que sobran: {temaSobrantes}");
+
 
             estudiantes = RandomizarLista(random, estudiantes);
             temas = RandomizarLista(random, temas);
 
+            List<string> equiposDistribuidos = new List<string>();
 
-            
-            
-            
-            //foreach (var item in estudiantes)
-            //{
-               // Console.WriteLine(item);
-            //}
+            for (int i = 0; i < nroDeEquipos; i++)
+            {
+                int miembrosPorEquipoReal = miembroPorEquipoSinSobrante;
+                int temasPorEquipoReal = temasPorEquipoSinSobrante;
+                if(random.Next(2) == 1 && miembroSobrantes > 0)
+                {
+                    miembrosPorEquipoReal++;
+                    miembroSobrantes--;
+                }
+                else if((nroDeEquipos - miembroSobrantes) == i)
+                {
+                    miembrosPorEquipoReal++;
+                    miembroSobrantes--;
+                }
+
+                if(random.Next(2) == 1 && temaSobrantes > 0)
+                {
+                    temasPorEquipoReal++;
+                    temaSobrantes--;
+                }
+                else if((nroDeEquipos - temaSobrantes) == i)
+                {
+                    temasPorEquipoReal++;
+                    temaSobrantes--;
+                }
+
+                equiposDistribuidos.Add($"Equipos {i+1} Integrantes: {miembrosPorEquipoReal} Temas: {temasPorEquipoReal}");
+                equiposDistribuidos.Add("\tNombre de los Integrantes:");
+                for (int j = 0; j < miembrosPorEquipoReal; j++)
+                {
+                    equiposDistribuidos.Add($"\t\t{estudiantes[0]}");
+                    estudiantes.RemoveAt(0);
+                }
+                equiposDistribuidos.Add("\tTemas: ");
+                for (int k = 0; k < temasPorEquipoReal; k++)
+                {
+                    equiposDistribuidos.Add($"\t\t{temas[0]}");
+                    temas.RemoveAt(0);
+                }
+            }
+
+                  
+            foreach (var item in equiposDistribuidos)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("[1] Si desea crear un documento .txt que guarde los equipos pulse 1");
+            Console.WriteLine("[SALIR] Escriba cualquier otra cosa");
+            string opcion = Console.ReadLine();
+            switch (opcion)
+            {
+                case("1"):
+                    File.WriteAllLines("grupos creados.txt", equiposDistribuidos);
+                    break;
+                default:
+                    break;
+            }
 
         }
         static List<string> RandomizarLista (Random random, List<string> lista)
