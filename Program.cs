131d0702a01be20a61704e061ee05851c7fa6810
DeFreitas-Samuel <52897285+DeFreitas-Samuel@users.noreply.github.com>
2020-11-09 13:46:15 -0400

    anadida la funcionalidad de randomizacion

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ namespace Organizador_de_equipos
     {
         static void Main(string[] args)
         {
+            Random random = new Random();
             Console.WriteLine("Por favor introduzca la ruta de el archivo donde estan los estudiantes");
             string filepath1 = Console.ReadLine();
             Console.WriteLine("Por favor introduzca la ruta del acrchivo donde estan los temas");
@@ -24,6 +25,32 @@ namespace Organizador_de_equipos
                 Console.WriteLine("El programa se va a cerrar");
                 Environment.Exit(0);
             }
+            
+
+            estudiantes = RandomizarLista(random, estudiantes);
+            temas = RandomizarLista(random, temas);
+
+
+            
+            
+            
+            //foreach (var item in estudiantes)
+            //{
+               // Console.WriteLine(item);
+            //}
+
+        }
+        static List<string> RandomizarLista (Random random, List<string> lista)
+        {
+            for (int i = 0; i < lista.Count; i++)
+            {
+                int tempRandom = random.Next(lista.Count);
+                string temp = lista[i];
+                lista[i] = lista[tempRandom];
+                lista[tempRandom] = temp;
+            }
+            return lista;
         }
     }
+    
 }
